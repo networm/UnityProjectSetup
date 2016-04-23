8a1ae47dbe12999d56aa5448944906168f8e7b39
networm <zzjbcn@gmail.com>
2016-04-23 22:53:19 +0800

    Add Windows support

--- unity.py
+++ unity.py
@@ -4,82 +4,105 @@
 import sys
 import os
 import shutil
+import platform
+import subprocess
 
 
-def generate(projectpath):
-    cmd = "/Applications/Unity/Unity.app/Contents/MacOS/Unity" + \
-          " -quit -batchmode " + \
-          " -logfile $stdout " + \
-          " -createProject " + projectpath
-    os.system(cmd)
+environment = {
+    "Windows":{
+        "Unity": r"C:\Program Files\Unity\Editor\Unity.exe",
+        "Git": r"C:\Program Files\Git\bin\git.exe"
+    },
+    "Darwin": {
+        "Unity": "/Applications/Unity/Unity.app/Contents/MacOS/Unity",
+        "Git": "git"
+    }
+}
 
 
-def setting(projectpath):
-    editordir = os.path.join(projectpath, "Assets/Editor")
+def generate(unity, projectpath):
+    cmd = [unity,
+           "-quit",
+           "-batchmode",
+           "-logfile",
+           "-createProject", projectpath]
+    subprocess.call(cmd)
+
+
+
+def setting(unity, sourcedir, projectpath):
+    editordir = os.path.abspath(os.path.join(projectpath, "Assets/Editor"))
     if not os.path.exists(editordir):
         os.mkdir(editordir)
-    shutil.copyfile("VCS.cs", editordir + "/VCS.cs")
+    shutil.copyfile(os.path.join(sourcedir, "VCS.cs"), os.path.join(editordir, "VCS.cs"))
 
-    cmd = "/Applications/Unity/Unity.app/Contents/MacOS/Unity" + \
-          " -quit -batchmode " + \
-          " -logfile $stdout " + \
-          " -executeMethod VCS.Setting"
-    os.system(cmd)
+    cmd = [unity,
+           "-quit",
+           "-batchmode",
+           "-logfile",
+           "-projectpath", projectpath,
+           "-executeMethod", "VCS.Setting"]
+    subprocess.call(cmd)
 
     shutil.rmtree(editordir)
     os.remove(editordir + ".meta")
 
 
-def gitinit(projectpath):
-    os.chdir(projectpath)
-    os.system("git init")
+def gitinit(git, projectpath):
+    subprocess.call([git, "-C", projectpath, "init"])
 
 
-def gitcommit(projectpath, summary):
-    os.chdir(projectpath)
-    os.system("git add --all")
-    os.system("git commit -m '{0}'".format(summary))
+def gitcommit(git, projectpath, summary):
+    subprocess.call([git, "-C", projectpath, "add", "--all"])
+    subprocess.call([git, "-C", projectpath, "commit", "-m", "{0}".format(summary)])
 
 
-def gitfile(sourcedir, projectpath):
-    shutil.copyfile(sourcedir + "/.gitignore", projectpath + "/.gitignore")
-    shutil.copyfile(sourcedir + "/.gitattributes", projectpath + "/.gitattributes")
+def gitfile(git, sourcedir, projectpath):
+    shutil.copyfile(os.path.join(sourcedir, ".gitignore"), os.path.join(projectpath, ".gitignore"))
+    shutil.copyfile(os.path.join(sourcedir, ".gitattributes"), os.path.join(projectpath, ".gitattributes"))
 
-    gitcommit(projectpath, "Initial commit")
+    gitcommit(git, projectpath, "Initial commit")
 
 
-def readme(sourcedir, projectpath, projectname):
+def readme(git, sourcedir, projectpath, projectname):
     content = "# {0}\n".format(projectname)
-    open(projectpath + "/README.md", 'w').write(content)
-    gitcommit(projectpath, "Add README")
+    open(os.path.join(projectpath, "README.md"), 'w').write(content)
+    gitcommit(git, projectpath, "Add README")
 
 
-def license(sourcedir, projectpath):
-    shutil.copyfile(sourcedir + "/LICENSE", projectpath + "/LICENSE")
-    gitcommit(projectpath, "Add LICENSE")
+def license(git, sourcedir, projectpath):
+    shutil.copyfile(os.path.join(sourcedir, "LICENSE"), os.path.join(projectpath, "LICENSE"))
+    gitcommit(git, projectpath, "Add LICENSE")
 
 
 def main(argv):
     projectname = ""
     projectpath = os.getcwd()
-    sourcedir = os.getcwd()
+    sourcedir = os.path.dirname(os.path.realpath(__file__))
 
     if len(argv) > 1:
+        projectname = os.path.basename(argv[1])
+        print(projectname)
         projectpath = argv[1]
     else:
         while os.path.exists(projectpath):
             projectname = raw_input("Unity project name: ")
-            dir = os.path.abspath(os.getcwd() + "/../")
+            dir = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir))
             projectpath = os.path.join(dir, projectname)
 
     print "Unity Project: " + projectpath
 
-    generate(projectpath)
-    setting(projectpath)
-    gitinit(projectpath)
-    gitfile(sourcedir, projectpath)
-    readme(sourcedir, projectpath, projectname)
-    license(sourcedir, projectpath)
+    projectpath = os.path.abspath(projectpath)
+
+    unity = environment[platform.system()]["Unity"]
+    git = environment[platform.system()]["Git"]
+
+    generate(unity, projectpath)
+    setting(unity, sourcedir, projectpath)
+    gitinit(git, projectpath)
+    gitfile(git, sourcedir, projectpath)
+    readme(git, sourcedir, projectpath, projectname)
+    license(git, sourcedir, projectpath)
 
     print "Job Done!"
 
